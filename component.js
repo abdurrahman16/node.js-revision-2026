1885a814a07152a8795ebb74272ff0bad68e9fff
Rahman Mohammad Abdur <47378897+abdurrahman16@users.noreply.github.com>
2025-12-30 12:44:29 +0900

    Add files via upload

---
+++ component.js
@@ -0,0 +1,22 @@
+let a =45;
+export let str='Babban';
+
+let newobj =
+{
+    name:'Rahat',
+    age:24,
+    national:'bd'
+}
+
+let arr=[9,9,8,5,6];
+
+let myfunc= () =>
+{
+    console.log('Herllo');
+}
+
+export class babu {
+    constructor(){
+        this.name='Babusssssssss';
+    }
+}
